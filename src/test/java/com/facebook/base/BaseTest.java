2d796033540075e715087e8a3423f719fe1dcd7e
Mohammed Molla <mjmolla@gmail.com>
2023-08-22 21:02:26 -0400

    key word driven framework

--- src/test/java/com/facebook/base/BaseTest.java
+++ src/test/java/com/facebook/base/BaseTest.java
@@ -5,44 +5,41 @@ import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import com.facebook.utilities.ReadPropertise;
+
 public class BaseTest {
-	WebDriver driver;
+	protected WebDriver driver;
+	ReadPropertise readPropertise;
 	
-	 String browser="edge";
-	
-	@BeforeMethod(groups={"smoke","regression"})
+	@BeforeMethod(groups = { "smoke", "regression" })
+
 	public void openApplication() {
+		readPropertise= new ReadPropertise();
+		String browser = readPropertise.getUrl();
+		
 		if (browser.equalsIgnoreCase("chrome")) {
-			chrome();
-		}else if(browser.equalsIgnoreCase("edge")) {
-			edge();
-		}else {System.out.println("wrong browser");}
-	}
-	
-	
-	
-	public WebDriver chrome() {
-		driver = new ChromeDriver();
+			driver = new ChromeDriver();
+		} else if (browser.equalsIgnoreCase("edge")) {
+			driver = new EdgeDriver();
+		} else if (browser.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		} else {
+			driver = new ChromeDriver();
+			System.out.println("chrome browser");
+		}
+		
+		driver.get(readPropertise.getUrl());
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("https://www.facebook.com/");
-		return driver;
 	}
-	
-	public WebDriver edge() {
-		driver = new EdgeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("https://www.facebook.com/");
-		return driver;
-	}
-	
-   @AfterMethod(groups={"smoke","regression"})
+
+	@AfterMethod(groups = { "smoke", "regression" })
 	public void closeApplication() {
-		if (driver !=null ) {
+		if (driver != null) {
 			driver.quit();
 		}
 	}
